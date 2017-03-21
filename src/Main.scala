2d2e9482b8444bfb9693077c68f98e9bbea38875
dblock <dblock@dblock.org>
2017-03-21 10:12:33 -0400

    Use lift-json.

--- src/Main.scala
+++ src/Main.scala
@@ -3,19 +3,26 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import com.fasterxml.jackson.module.scala.experimental.ScalaObjectMapper
 
+import net.liftweb.json._
 /**
   * Created by dblock on 3/21/17.
   */
 object Main {
+  def parseWithJackson(json: BufferedSource): Map[String, Object] = {
+    val mapper = new ObjectMapper() with ScalaObjectMapper
+    mapper.registerModule(DefaultScalaModule)
+    mapper.readValue[Map[String, Object]](json.reader())
+  }
+
+  def parseWithLiftweb(json: BufferedSource): JValue = {
+    net.liftweb.json.parse(json.mkString)
+  }
+
   def main(args: Array[String]): Unit = {
     val filename = args.head
     // read
     println(s"Reading ${args.head} ...")
-    val json = Source.fromFile(filename)
-    // parse
-    val mapper = new ObjectMapper() with ScalaObjectMapper
-    mapper.registerModule(DefaultScalaModule)
-    val parsedJson = mapper.readValue[Map[String, Object]](json.reader())
-    println(parsedJson)
+    println(parseWithJackson(Source.fromFile(filename)))
+    println(parseWithLiftweb(Source.fromFile(filename)))
   }
 }
